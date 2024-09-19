9aab292235bdb0e4200fce391335816571fb8d4b
Anas Alhariri <147917604+AlhaririAnas@users.noreply.github.com>
2024-09-19 22:21:44 +0200

    Added main.py

---
+++ resources/main.py
@@ -0,0 +1,148 @@
+import os
+import torch
+import pickle
+from tqdm import tqdm
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from collections import defaultdict
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+from resources.generator import ImageGenerator
+from resources.metadata_reader import (
+    create_database,
+    get_metadata,
+    save_metadata_in_database,
+    get_last_entry,
+    get_filename_from_id,
+)
+from resources.similarity import get_similarities
+from app.app import app, start_app
+
+parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("-m", "--metadata", action="store_true")
+
+parser.add_argument("-s", "--similarity", action="store_true")
+
+parser.add_argument("-p", "--path", action="store", default="C:/")
+
+parser.add_argument("-d", "--device", type=str, default=None, help="Device to use: cuda or cpu")
+
+parser.add_argument("--pkl_file", action="store", default="similarities.pkl")
+
+parser.add_argument("--checkpoint", type=int, default=100)
+
+parser.add_argument("--debug", action="store_true")
+
+args = parser.parse_args()
+
+
+def run(args):
+    """
+    Runs the main image processing pipeline based on the provided arguments.
+
+    Args:
+        args: The arguments containing device information, file paths, and processing options.
+
+    Returns:
+        None
+    """
+    if args.device is None:
+        args.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    create_database()
+    try:
+        with open(args.pkl_file, "rb") as f:
+            similarities = pickle.load(f)
+    except FileNotFoundError:
+        similarities = defaultdict()
+        similarities[0] = None
+
+    last_db_id = get_last_entry()
+    last_sim_id = max(similarities.keys())
+    id = min(last_sim_id, last_db_id)
+
+    if id == 0:
+        starting_path = None
+    else:
+        starting_path = os.path.join(args.path, get_filename_from_id(id))
+    img_gen = ImageGenerator(args.path).image_generator(starting_path=starting_path)
+
+    for img in tqdm(img_gen, total=2313, initial=id):
+        id += 1
+        if args.metadata and last_db_id < id:
+            metadata = get_metadata(img)
+            save_metadata_in_database(metadata)
+        if args.similarity and last_sim_id < id:
+            try:
+                similarities[id] = get_similarities(img, args)
+            except OSError:
+                continue
+            if id % args.checkpoint == 0:
+                with open(args.pkl_file, "wb") as f:
+                    pickle.dump(similarities, f)
+    with open(args.pkl_file, "wb") as f:
+        pickle.dump(similarities, f)
+
+
+def create_and_save_clustering_model(vectors, vector_ids, filename, clusters):
+    """
+    Creates and saves a clustering model based on the input vectors.
+
+    Parameters:
+        vectors (array): The input vectors for clustering.
+        vector_ids (array): The IDs corresponding to the input vectors.
+        filename (str): The name of the file to save the clustering model.
+        clusters (int): The number of clusters to create.
+
+    Returns:
+        None
+    """
+    scaler = StandardScaler()
+    vectors_scaled = scaler.fit_transform(vectors)
+
+    model = KMeans(n_clusters=clusters, random_state=0)
+    model.fit(vectors_scaled)
+
+    with open(filename, "wb") as f:
+        pickle.dump({"model": model, "scaler": scaler, "vector_ids": vector_ids}, f)
+    print(f"Clustering model saved to {filename}")
+
+
+def load_pkl_files():
+    """
+    A function to load pickle files containing similarities and embedding clusters.
+    If the files are not found, it creates the clusters using the create_and_save_clustering_model function.
+    """
+    print("Loading similarities from pickle file...")
+    try:
+        with open(args.pkl_file, "rb") as f:
+            similarities = pickle.load(f)
+            app.config["SIMILARITIES"] = similarities
+    except FileNotFoundError:
+        raise ValueError("No similarities found! Run the script with the -s flag.")
+    
+    if not os.path.exists("embedding_cluster.pkl"):
+        print("No embedding cluster found. Creating...")
+        create_and_save_clustering_model(
+            [similarities[v][1] for v in similarities.keys()],
+            [v for v in similarities.keys()],
+            filename="embedding_cluster.pkl",
+            clusters=10, #should be adjusted
+        )
+    
+    with open("embedding_cluster.pkl", "rb") as f:
+        app.config["EMBEDDING_CLUSTER"] = pickle.load(f)
+
+    print("Done!")
+
+
+if __name__ == "__main__":
+    if args.metadata or args.similarity:
+        run(args)
+    else:
+        app.config["ARGS"] = args
+        load_pkl_files()
+        if args.debug:
+            app.run(debug=True)
+        else:
+            start_app()
